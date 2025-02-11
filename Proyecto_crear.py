153117428300278b1442e69221d9fde6750a88b9
Juan Antonio <zampagamer064@gmail.com>
2025-02-11 11:14:40 +0100

    Update Proyecto_crear.py
    He eliminado errores de la sintaxis y termine la tabla de prestamos

--- Proyecto_crear.py
+++ Proyecto_crear.py
@@ -5,7 +5,7 @@ cursor = conexion.cursor()
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Usuarios (
-        id_usuario INTERGER PRIMARY KEY AUTOINCREMENT,
+        id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
         nombre TEXT NOT NULL,
         email TEXT UNIQUE NOT NULL,
         telefono TEXT
@@ -14,7 +14,7 @@ cursor.execute('''
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS Videojuegos (
-        id_videjuego INTERGER PRIMARY KEY AUTOINCREMENT,
+        id_videjuego INTEGER PRIMARY KEY AUTOINCREMENT,
         titulo TEXT NOT NULL,
         plataforma TEXT UNIQUE NOT NULL,
         stock INTERGER NOT NULL
